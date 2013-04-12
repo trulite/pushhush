1a89ece58944eb4b0c42365e22adb52557a88368
Satya Prakash <satyaprakash007@gmail.com>
2013-04-12 16:52:20 +0530

    Generating java ready certs

--- genkeys.sh
+++ genkeys.sh
@@ -1,3 +1,5 @@
 openssl genrsa -out privatekey.pem 1024 
 openssl req -new -key privatekey.pem -out certrequest.csr 
 openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+openssl pkcs8 -topk8 -nocrypt -in privatekey.pem -inform PEM -out privatekey.der -outform DER
+openssl x509 -in certificate.pem -inform PEM -out certificate.der -outform DER
